549c90b9133df8d18bc1aca43f2ff9de010e6554
Mustafa Imam <mustafa.imam@ontariotechu.net>
2023-11-03 23:41:17 -0400

    fixed pages, error page messes up need to fix

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-let session = require('express-session');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
 let app = express();
-
-// Session setup
-app.use(session({
-  secret: 'secret_key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,7 +24,46 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catches 404 and forwards it to the error handler
+// POST route from contact form
+app.post('/send', (req, res) => {
+  // Basic security check
+  if(req.body.securityCheck !== '4') {
+    res.send("Security check failed.");
+    return;
+  }
+
+  // Creates a SMTP transporter object
+  let transporter = nodemailer.createTransport({
+    service: 'Outlook', 
+    auth: {
+      user: 'mustafa-imam@outlook.com', // Your authenticated email
+      pass: 'pfdduwagkdnpqdxy' // Your app password
+    }
+  });
+
+  // Message object
+  let message = {
+    from: 'mustafa-imam@outlook.com', // Your authenticated email
+    to: 'mustafa-imam@outlook.com', // Your email
+    subject: 'New Message from ' + req.body.email, // Include user's email in subject
+    text: 'Name: ' + req.body.name + '\nEmail: ' + req.body.email + '\nMessage: ' + req.body.comments,
+    html: '<p><b>Name:</b> ' + req.body.name + '</p><p><b>Email:</b> ' + req.body.email + '</p><p><b>Message:</b> ' + req.body.comments + '</p>'
+  };
+
+  // sends mail with defined transport object
+  transporter.sendMail(message, (error, info) => {
+    if (error) {
+      res.send("Error occurred.");
+      console.log('Error occurred. ' + error.message);
+      return process.exit(1);
+    }
+
+    console.log('Message sent: %s', info.messageId);
+    res.send("Message sent successfully!");
+  });
+});
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -44,17 +74,13 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // renders the error page with custom message if available
+  // renders the error page
   res.status(err.status || 500);
-  res.render('error', {
-    title: err.status === 404 ? "404: File Not Found" : "Error",
-    message: req.session.customErrorMessage || err.message,
-    error: err,
-    success: false
-  });
-
-  // Clears custom error message after displaying it
-  req.session.customErrorMessage = null;
+  res.render('error', 
+  {
+    title: "Error"
+  }
+  );
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,18 @@
-// POST route from contact form with token check
-router.post('/send', (req, res, next) => {
-  // Check if the session token matches
-  if(req.session.contactFormToken !== 'unique_token') {
-    req.session.customErrorMessage = "Unauthorized access to send route.";
-    return res.redirect('/'); // Redirect to home if token doesn't match
-  }
+var express = require('express');
+var router = express.Router();
 
-  // Basic security check (if you have a captcha or something similar)
-  if(req.body.securityCheck !== '4') {
-    req.session.customErrorMessage = "Security check failed.";
-    return res.redirect('/');
-  }
-
-  // Creates a SMTP transporter object
-  let transporter = nodemailer.createTransport({
-    service: 'Outlook', 
-    auth: {
-      user: 'mustafa-imam@outlook.com', // my authenticated email
-      pass: 'pfdduwagkdnpqdxy' // my app password
-    }
-  });
-
-  // Message object
-  let message = {
-    from: 'mustafa-imam@outlook.com',
-    to: 'mustafa-imam@outlook.com',
-    subject: 'New Message from ' + req.body.email, // Includes user's email in subject
-    text: 'Name: ' + req.body.name + '\nEmail: ' + req.body.email + '\nMessage: ' + req.body.comments,
-    html: '<p><b>Name:</b> ' + req.body.name + '</p><p><b>Email:</b> ' + req.body.email + '</p><p><b>Message:</b> ' + req.body.comments + '</p>'
-  };
-
-  // sends mail with defined transport object
-  transporter.sendMail(message, (error, info) => {
-    if (error) {
-      req.session.customErrorMessage = 'There was an error sending your message, please try again.';
-      res.redirect('/');
-    } else {
-      // Clear the token and render success page
-      req.session.contactFormToken = null;
-      res.render('success', {
-        title: "Message Sent",
-        message: "Thank you for submitting your message! I will try my best to get back to you. If I don't get back to you in a month, that probably means I forgot to read your email so feel free to send me another message if you want. Please don't spam me, thanks!"
-      });
-    }
-  });
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
+});
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
