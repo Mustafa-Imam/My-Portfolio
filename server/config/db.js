21e6d16e23469d71b16acfec4520828c0df82154
Mustafa Imam <mustafa.imam@ontariotechu.net>
2023-11-15 22:24:26 -0500

    fixed app.css file and mongo URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "URI": "mongodb+srv://mustafa-imam:KLNCAA0IBpRVW7AJ@cluster0.dm8lw3x.mongodb.net/BookStore"
+    "URI": "mongodb+srv://mustafaimam:ABePiivDeIF1sQtO@cluster0.wiqcxr9.mongodb.net/"
   };
   
