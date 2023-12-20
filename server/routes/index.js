2169de75ec1eaf2ae837ea9f7f0806a48ffe38ab
Mustafa Imam <mustafa.imam@ontariotechu.net>
2023-12-19 22:50:00 -0500

    yo bro

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const passport = require('passport');
-let userModel = require('../models/user');
-let User = userModel.User;
 
 // Nav Bar Routes
 router.get('/', function(req, res, next) {
-    res.render('home', { title: 'Home', displayName: req.user ? req.user.displayName : '' });
+    res.render('home', { title: 'Home' });
 });
 router.get('/aboutme', function(req, res, next) {
-    res.render('aboutme', { title: 'About Me', displayName: req.user ? req.user.displayName : '' });
+    res.render('aboutme', { title: 'About Me' });
 });
 router.get('/projects', function(req, res, next) {
-    res.render('projects', { title: 'Projects', displayName: req.user ? req.user.displayName : '' });
+    res.render('projects', { title: 'Projects' });
 });
 router.get('/contact', function(req, res, next) {
-    res.render('contact', { title: 'Contact', displayName: req.user ? req.user.displayName : '' });
+    res.render('contact', { title: 'Contact' });
 });
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-  });
 
 module.exports = router;
